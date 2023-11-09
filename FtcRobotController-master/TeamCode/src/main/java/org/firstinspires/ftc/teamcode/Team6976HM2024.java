0fa49297b1c1b4b61e73e9bead754a5206b1c47a
Void975 <150174899+Void975@users.noreply.github.com>
2023-11-09 16:50:29 -0600

    Renamed Intake Servo

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team6976HM2024.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team6976HM2024.java
@@ -80,7 +80,7 @@ public class Team6976HM2024 {
         Arm2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         Arm2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        Intake = hwMap.get(Servo.class, "Servo1");
+        Intake = hwMap.get(Servo.class, "Intake");
 
         //Arm = hwMap.get(DcMotor.class, "Arm");
 

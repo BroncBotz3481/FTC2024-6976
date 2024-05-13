171ec147df616b5c7432ffa7f7a1ddd7ad329561
Nobody <nobodyhere618@gmail.com>
2024-05-13 13:05:17 -0500

    auto

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team6976AutoPropMiddleRed.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team6976AutoPropMiddleRed.java
@@ -10,61 +10,94 @@ public class Team6976AutoPropMiddleRed extends LinearOpMode {
 
     Team6976HM2024 robot = new Team6976HM2024();
     ElapsedTime Time = new ElapsedTime();
-
+    double multy = 0.5;
 
     @Override
     public void runOpMode() {
-        robot.Map(hardwareMap);
-        robot.Intake.setPosition(0.3);
-        robot.Intake2.setPosition(0);
-        waitForStart();
-        moveForward(0.4 , 1650); //moves forward
-        moveBackward(0.4,1600); //backwards
-        moveRight(0.5,2100);
-        moveForward(0.4 , 500); //moves forward
+        double distance = 36; //Distance in inches to strafe
+        multy = 0.5; //Power setting to all motors
+        robot.DriveLeftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.DriveLeftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        double tick = (distance * 537.7)/(4 * Math.PI);
+        Time.reset();
+        robot.DriveRightFront.setPower(multy); //Setting the power to (multy) variable created above (-multy for red side)
+        robot.DriveLeftFront.setPower(multy); //Link to Wheel Direction Mapping Below
+        robot.DriveRightBack.setPower(multy); //https://gm0.org/en/latest/docs/software/tutorials/mecanum-drive.html
+        robot.DriveLeftBack.setPower(multy);
 
 
+        while(opModeIsActive() && Time.milliseconds() < 2000 && robot.DriveLeftFront.getCurrentPosition() < tick) { //If Encoder is outputting incorrectly, motor will automatically stop if time in miliseconds has been reached
+            telemetry.addData("Encoder Val", robot.DriveLeftFront.getCurrentPosition()); //Printing Telemtry values to the phone
+            telemetry.update(); //Constantly updates telemetry to the phone
+        }
 
-    }
-    public void moveForward (double power, int time){
-        robot.DriveRightFront.setPower(power);
-        robot.DriveLeftFront.setPower(power);
-        robot.DriveRightBack.setPower(-power);
-        robot.DriveLeftBack.setPower(-power);
-        sleep(time);
+        robot.DriveRightFront.setPower(0); //Sets power to all motors to 0 after desired distance has been reached
+        robot.DriveLeftFront.setPower(0);
+        robot.DriveRightBack.setPower(0);
+        robot.DriveLeftBack.setPower(0);
+        robot.DriveLeftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); //Stops and resets the encoder to the 0 value for next use
+        robot.DriveLeftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        sleep(1000);
+
+        double backDistance = 36;
+        robot.DriveLeftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.DriveLeftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Calculate target encoder tick count for moving backward
+        double backwardTick = (backDistance * 537.7) / (4 * Math.PI);
+
+        // Reset elapsed time
+        Time.reset();
+
+        // Set power for all motors to move backward
+        robot.DriveRightFront.setPower(-multy);
+        robot.DriveLeftFront.setPower(-multy);
+        robot.DriveRightBack.setPower(-multy);
+        robot.DriveLeftBack.setPower(-multy);
+
+// Continue until the OpMode is active, elapsed time is less than 2000 milliseconds, and the left front motor's encoder position is greater than or equal to the target tick count
+        while(opModeIsActive() && Time.milliseconds() < 2000 && robot.DriveLeftFront.getCurrentPosition() > -backwardTick) {
+            telemetry.addData("Encoder Val", robot.DriveLeftFront.getCurrentPosition());
+            telemetry.update();
+        }
+
         robot.DriveRightFront.setPower(0);
         robot.DriveLeftFront.setPower(0);
         robot.DriveRightBack.setPower(0);
         robot.DriveLeftBack.setPower(0);
-    }
-    public void moveBackward (double power, int time){
-        robot.DriveRightFront.setPower(-power);
-        robot.DriveLeftFront.setPower(-power);
-        robot.DriveRightBack.setPower(power);
-        robot.DriveLeftBack.setPower(power);
-        sleep(time);
-        robot.DriveRightFront.setPower(0);
-        robot.DriveLeftFront.setPower(0);
-        robot.DriveRightBack.setPower(0);
-        robot.DriveLeftBack.setPower(0);
-    }
-    public void moveRight (double power, int time){
-        robot.DriveRightFront.setPower(-power );
-        robot.DriveLeftFront.setPower(power);
-        robot.DriveRightBack.setPower(-power);
-        robot.DriveLeftBack.setPower(power );
-        sleep(time);
-        robot.DriveRightFront.setPower(0);
-        robot.DriveLeftFront.setPower(0);
-        robot.DriveRightBack.setPower(0);
-        robot.DriveLeftBack.setPower(0);
-    }
-    public void moveArm (double power, int time){
-        robot.Arm2.setPower(power);
-        robot.Arm2b.setPower(power);
-        sleep(time);
-        robot.Arm2.setPower(0);
-        robot.Arm2b.setPower(0);
+        robot.DriveLeftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.DriveLeftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        sleep(1000);
 
+
+        double sideDistance = 40;
+        robot.DriveLeftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.DriveLeftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Calculate target encoder tick count for moving backward
+        double sideTick = (sideDistance * 537.7) / (4 * Math.PI);
+
+        // Reset elapsed time
+        Time.reset();
+
+        // Set power for all motors to move backward
+        robot.DriveRightFront.setPower(-multy);
+        robot.DriveLeftFront.setPower(multy);
+        robot.DriveRightBack.setPower(multy);
+        robot.DriveLeftBack.setPower(-multy);
+
+
+        while(opModeIsActive() && Time.milliseconds() < 2000 && robot.DriveLeftFront.getCurrentPosition() > sideTick) {
+            telemetry.addData("Encoder Val", robot.DriveLeftFront.getCurrentPosition());
+            telemetry.update();
+        }
+
+        robot.DriveRightFront.setPower(0);
+        robot.DriveLeftFront.setPower(0);
+        robot.DriveRightBack.setPower(0);
+        robot.DriveLeftBack.setPower(0);
+        robot.DriveLeftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.DriveLeftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        sleep(1000);
     }
 }
